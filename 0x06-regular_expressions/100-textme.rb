949c04e3ee47a7859456b2608403d4a21b7afba3
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2023-08-02 03:00:55 +0000

    Fix task 8, project 0x06

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from:).*?(?=\])|(?<=to:).*?(?=\])|(?<=flags:).*?(?=\])/).join
+#puts ARGV[0].scan(/(?<=from:).*?(?=\])|(?<=to:).*?(?=\])|(?<=flags:).*?(?=\])/).join
+printf("%s,%s,%s",ARGV[0].scan(/(?<=from:).*?(?=\])/).join, ARGV[0].scan(/(?<=to:).*?(?=\])/).join, ARGV[0].scan(/(?<=flags:).*?(?=\])/).join)
