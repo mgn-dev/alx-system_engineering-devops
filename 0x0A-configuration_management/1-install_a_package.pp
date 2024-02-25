7e4045531d05e43a83365cd61726bbca90dc2a6f
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-02-26 00:13:29 +0200

    Do task 1, project 0x0A, Configuration Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # puppet resources that install flask from pip3
 
-exec { 'install_flask':
-  command   => '/usr/bin/pip3 install Flask==2.1.0',
+package { 'python3-flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
