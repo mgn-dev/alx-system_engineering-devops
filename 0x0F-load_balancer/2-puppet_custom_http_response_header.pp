7e73de7afba080b8e382f141df1c7745649d2690
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-05 23:39:41 +0200

    Do task 2, project 0x0F, Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,48 +1,24 @@
 # Puppet manifest to configure NGINX redirect on Ubuntu server
 
 exec { 'update system':
-  command => '/usr/bin/apt-get update',
+        command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['update system'],
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Add new location directive
-$redirect = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-$directive = "location /redirect { return 301 ${redirect}; }"
-$nginx_conf_file = '/etc/nginx/sites-available/default'
-
-exec { 'add_location_directive':
-  command => "/bin/bash -c 'if ! grep -q \"${directive}\" \"${nginx_conf_file}\"; then sed -i \"/^\\s*location \\/ {/ { :1; /}/!{N;b1}; s|}|&\\n\\t${directive}|\" \"${nginx_conf_file}\"; fi'",
-  path    => ['/bin', '/usr/bin'],
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Add error directive
-$directive = 'error_page 404 /error_404.html;'
-$nginx_conf_file = '/etc/nginx/sites-available/default'
-
-exec { 'add_error_directive':
-  command => "/bin/bash -c 'if ! grep -q \"${directive}\" \"${nginx_conf_file}\"; then sed -i \"/^\\s*location \\/ {/ { :1; /}/!{N;b1}; s|}|&\\n\\t${directive}|\" \"${nginx_conf_file}\"; fi'",
-  path    => ['/bin', '/usr/bin'],
-}
-
-# Add header directive
-$directive = 'add_header X-Served-By $hostname;'
-$nginx_conf_file = '/etc/nginx/sites-available/default'
-
-exec { 'add_header_directive':
-  command => "/bin/bash -c 'if ! grep -q \"${directive}\" \"${nginx_conf_file}\"; then sed -i \"/^\\s*location \\/ {/a\\ \\t\\t${directive}\" \"${nginx_conf_file}\"; fi'",
-  path    => ['/bin', '/usr/bin'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
