75180c716ed241976edbb5b4ffc7fe0dd343df6c
Mojalefa Nkwana <126557401+mgn-dev@users.noreply.github.com>
2024-03-05 23:37:31 +0200

    Do task 2, project 0x0F, Load Balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,47 +1,27 @@
 # Puppet manifest to configure NGINX redirect on Ubuntu server
 
-# Install NGINX package
+exec { 'update system':
+  command => '/usr/bin/apt-get update',
+}
+
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['update system'],
 }
 
-# Allow directory ownership for the current user
-file { '/var/www/html':
-  ensure  => directory,
-  owner   => $user,
-  group   => $user,
-  recurse => true,
-}
-
-file { '/etc/nginx':
-  ensure  => directory,
-  owner   => $user,
-  group   => $user,
-  recurse => true,
-}
-
-# Create index.html file
 file { '/var/www/html/index.html':
   ensure  => file,
-  content => "Hello World!\n",
-  require => File['/var/www/html']
-}
-
-# Create error_404.html file
-file { '/var/www/html/error_404.html':
-  ensure  => file,
-  content => "Ceci n'est pas une page\n",
-  require => File['/var/www/html']
+  content => 'Hello World!',
 }
 
 # Add new location directive
 $redirect = 'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-$directive = "location /redirect { return 301 $redirect; }"
+$directive = "location /redirect { return 301 ${redirect}; }"
 $nginx_conf_file = '/etc/nginx/sites-available/default'
 
 exec { 'add_location_directive':
-  command => "/bin/bash -c 'if ! grep -q \"$directive\" \"$nginx_conf_file\"; then sed -i \"/^\t*location \\/ {/ { :1; /}/!{N;b1}; s|}|&\\n\\t$directive| }\" \"$nginx_conf_file\"; fi'",
-  path    => '/usr/bin:/bin',
+  command => "/bin/bash -c 'if ! grep -q \"${directive}\" \"${nginx_conf_file}\"; then sed -i \"/^\\s*location \\/ {/ { :1; /}/!{N;b1}; s|}|&\\n\\t${directive}|\" \"${nginx_conf_file}\"; fi'",
+  path    => ['/bin', '/usr/bin'],
 }
 
 # Add error directive
@@ -49,8 +29,8 @@ $directive = 'error_page 404 /error_404.html;'
 $nginx_conf_file = '/etc/nginx/sites-available/default'
 
 exec { 'add_error_directive':
-  command => "/bin/bash -c 'if ! grep -q \"$directive\" \"$nginx_conf_file\"; then sed -i \"/^\t*location \\/ {/ { :1; /}/!{N;b1}; s|}|&\\n\\t$directive| }\" \"$nginx_conf_file\"; fi'",
-  path    => '/usr/bin:/bin',
+  command => "/bin/bash -c 'if ! grep -q \"${directive}\" \"${nginx_conf_file}\"; then sed -i \"/^\\s*location \\/ {/ { :1; /}/!{N;b1}; s|}|&\\n\\t${directive}|\" \"${nginx_conf_file}\"; fi'",
+  path    => ['/bin', '/usr/bin'],
 }
 
 # Add header directive
@@ -58,19 +38,11 @@ $directive = 'add_header X-Served-By $hostname;'
 $nginx_conf_file = '/etc/nginx/sites-available/default'
 
 exec { 'add_header_directive':
-  command => "/bin/bash -c 'if ! grep -q \"$directive\" \"$nginx_conf_file\"; then sed -i \"/^\tlocation \\/ {/a\\ \\t\\t$directive\" \"$nginx_conf_file\"; fi'",
-  path    => '/usr/bin:/bin',
+  command => "/bin/bash -c 'if ! grep -q \"${directive}\" \"${nginx_conf_file}\"; then sed -i \"/^\\s*location \\/ {/a\\ \\t\\t${directive}\" \"${nginx_conf_file}\"; fi'",
+  path    => ['/bin', '/usr/bin'],
 }
 
-# Restart NGINX
 service { 'nginx':
   ensure  => running,
-  enable  => true,
-  require => Exec['add_location_directive', 'add_error_directive', 'add_header_directive'],
-  notify  => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
-  command     => 'sudo service nginx restart',
-  refreshonly => true,
+  require => Package['nginx'],
 }
