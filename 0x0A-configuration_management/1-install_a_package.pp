4ee901f84fcb7f72028bba4f11c1b851c91046b1
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-06-29 01:58:16 +0200

    Do task 1, project 0x0A, Config Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # puppet resources that install flask from pip3
 
-exec { 'install python packages':
-    command   => 'pip3 install flask flask_restful apiai',
-    path => ['/usr/bin/'],
-    unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
