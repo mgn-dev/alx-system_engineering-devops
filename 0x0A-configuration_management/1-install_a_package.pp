b42063390984e8eb2a48b9232273224a6b7c1ad4
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-06-29 01:47:38 +0200

    Do task 1, project 0x0A, Config Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # puppet resources that install flask from pip3
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => 'pip3 install flask==2.1.0',
   path    => ['/bin', '/usr/bin'],
 }
