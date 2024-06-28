d0a48eaf33aaadea09f260e11c448234a37ade8d
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-06-29 01:36:36 +0200

    Do task 1, project 0x0A, Config Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # puppet resources that install flask from pip3
 
-package { 'python3-pip':
+package { 'python-pip':
   ensure => installed,
 }
 
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip',
+  require  => Package['python-pip'],
 }
