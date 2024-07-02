40fd3c8a255c56316fe54ef5a492f0403512d13b
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-07-02 19:33:49 +0200

    Do task 0-5, project 0x0C, Web Server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# puppet script to configure ubuntu server
+
+package {'nginx':
+  ensure => 'installed',
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
+file {'/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+exec { 'redirect_me':
+  command  => "sudo sed -i '53i\\tlocation /redirect { return 301 \$REDIRECT_URL; }' /etc/nginx/sites-available/default",
+  provider => 'shell',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+}
