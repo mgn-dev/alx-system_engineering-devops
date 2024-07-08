d8b97f2c0ed11fae7b8a02ea4f124ec430973716
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-07-09 01:43:46 +0200

    Do task 0-2, project 0x0F, Load Balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Puppet manifest to configure NGINX redirect on Ubuntu server
+
+exec { 'update system':
+    command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+    ensure  => 'installed',
+    require => Exec['update system']
+}
+
+file {'/var/www/html/index.html':
+    content => 'Hello World!'
+}
+
+exec {'HTTP header':
+    command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+    provider => 'shell'
+}
+
+service {'nginx':
+    ensure  => running,
+    require => Package['nginx']
+}
