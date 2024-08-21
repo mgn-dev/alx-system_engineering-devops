9dffccab795269f4ef0e22f24a1cdd240c64b321
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-08-21 08:56:21 +0200

    Do task 0, project 0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,16 +11,10 @@ def number_of_subscribers(subreddit):
     Queries the Reddit API and returns the number of subscribers
     for a given subreddit. If the subreddit is invalid, returns 0.
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent':
-               'Python:subreddit.subscriber.count:v1.0.0 (by /u/yourusername)'}
-
+    user = {'User-Agent': '0-subs:v1.0.0'}
+    url = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                       headers=user).json()
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
