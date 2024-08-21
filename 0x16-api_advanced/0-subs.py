46805528226251e7122673c20f7884bd0bf22f8d
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-08-21 09:25:39 +0200

    Do task 0, project 0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': '0-subs:v1.0.0'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
