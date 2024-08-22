c22527fa11f26ec098aa16cabd975ec6ab6b1d46
emjay026 <126557401+mgn-dev@users.noreply.github.com>
2024-08-22 20:24:55 +0200

    Do task 0, project 0x1B. Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# puppet script that updates nginx number of open files
+
+exec {'update open files limit':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
